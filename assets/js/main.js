dd8741b801d0e0559fc0bf2850f25b50b0cf4b34
Ruben <rubenpinto654@gmail.com>
2022-01-10 23:50:13 +0000

    Added Info and Countdown

--- assets/js/main.js
+++ assets/js/main.js
@@ -42,6 +42,36 @@
 
 })();
 
+
+(function () {
+  const second = 1000,
+        minute = second * 60,
+        hour = minute * 60,
+        day = hour * 24;
+  
+  const countDown = new Date("Jun 10, 2022 00:00:01").getTime(),
+      x = setInterval(function() {    
+
+        const now = new Date().getTime(),
+              distance = countDown - now;
+
+        document.getElementById("days").innerText = Math.floor(distance / (day)),
+          document.getElementById("hours").innerText = Math.floor((distance % (day)) / (hour)),
+          document.getElementById("minutes").innerText = Math.floor((distance % (hour)) / (minute)),
+          document.getElementById("seconds").innerText = Math.floor((distance % (minute)) / second);
+
+        //do something later when date is reached
+        if (distance < 0) {
+          document.getElementById("days").innerText = 0,
+          document.getElementById("hours").innerText = 0,
+          document.getElementById("minutes").innerText = 0,
+          document.getElementById("seconds").innerText = 0,
+          clearInterval(x);
+        }
+        //seconds
+      }, 0)
+  }());
+
 // ====== scroll top js
 function scrollTo(element, to = 0, duration= 1) {
 
